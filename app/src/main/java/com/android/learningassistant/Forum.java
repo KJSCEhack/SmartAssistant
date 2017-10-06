2928865db69f95dd8338d1a4296dfad0b3e2665c
Ronak Doshi <ronak01doshi@gmail.com>
2017-10-06 19:40:34 +0530

    Initial Commit

---
+++ app/src/main/java/com/android/learningassistant/Forum.java
@@ -0,0 +1,89 @@
+package com.android.learningassistant;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Forum.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Forum#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Forum extends Fragment {
+
+    List<String> spinnerArray =  new ArrayList<String>();
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public Forum() {
+        // Required empty public constructor
+    }
+
+
+    public static Forum newInstance(String param1, String param2) {
+        Forum fragment = new Forum();
+        Bundle args = new Bundle();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_forum, container, false);
+
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/android/learningassistant/Navigation.java
@@ -0,0 +1,228 @@
+package com.android.learningassistant;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ames.books.BookListActivity;
+import com.clover_studio.spikachatmodule.ChatActivity;
+import com.clover_studio.spikachatmodule.models.Config;
+import com.clover_studio.spikachatmodule.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.ionicframework.attendance914014.MyActivity;
+
+public class Navigation extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private FirebaseAuth auth;
+    EditText room;
+    boolean doubleBackToExitPressedOnce = false;
+    String id;
+    Toolbar toolbar = null;
+    NavigationView navigationView=null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        auth=FirebaseAuth.getInstance();
+
+        Courses fragment = new Courses();
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+        toolbar.setTitle("Courses");
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(view.getId()==R.id.fab) {
+                    setContentView(R.layout.choose_room);
+                    Button join = (Button) findViewById(R.id.button3);
+
+                    join.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            if (view.getId() == R.id.button3) {
+                                room = (EditText) findViewById(R.id.room_id);
+                                id = room.getText().toString();
+                                if (id.length() < 1) {
+                                    Snackbar snackbar = Snackbar
+                                            .make(view, "Please Enter the Room id", Snackbar.LENGTH_LONG);
+
+                                    snackbar.show();
+
+                                } else {
+                                    User user = new User();
+                                    user.roomID = id;
+                                    user.userID = auth.getCurrentUser().getDisplayName().toString();
+                                    user.name = auth.getCurrentUser().getDisplayName().toString();
+                                    user.avatarURL = auth.getCurrentUser().getPhotoUrl().toString();
+                                    Config config = new Config();
+                                    config.apiBaseUrl = "http://192.168.122.204:80/spika/v1/";
+                                    config.socketUrl = "http://192.168.122.204:80/spika";
+
+                                    ChatActivity.startChatActivityWithConfig(Navigation.this, user, config);
+                                }
+                            }
+                        }
+                    });
+                }
+
+
+
+                }
+
+        });
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        startActivity(new Intent(Navigation.this,Navigation.class));
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.attendance) {
+            Intent intent = new Intent(this,MyActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.courses) {
+            Courses fragment = new Courses();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            toolbar.setTitle("Courses");
+
+
+        } else if (id == R.id.e_library) {
+           startActivity(new Intent(Navigation.this, BookListActivity.class));
+        } else if (id == R.id.jobs) {
+            Jobs_Internships fragment = new Jobs_Internships();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            toolbar.setTitle("Jobs/Internships");
+
+
+        } else if (id == R.id.projects) {
+            Projects fragment = new Projects();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            toolbar.setTitle("Projects");
+
+
+        } else if (id == R.id.games) {
+            Games fragment = new Games();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            toolbar.setTitle("Games");
+
+
+        } else if (id == R.id.workshops) {
+            UpcomingWorkshops fragment = new UpcomingWorkshops();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            toolbar.setTitle("Upcoming Workshops");
+
+        } else if (id == R.id.about_us) {
+            AboutUs fragment = new AboutUs();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            toolbar.setTitle("About Us");
+
+
+        }else if (id == R.id.forum) {
+            Forum fragment = new Forum();
+            android.support.v4.app.FragmentTransaction fragmentTransaction =
+                    getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            toolbar.setTitle("Forum");
+
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
